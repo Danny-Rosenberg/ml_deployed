2e14060e56ac49d8c50bf1a8ff327413fbfb1e5e
Daniel Rosenberg <rosenbergd36@gmail.com>
2018-08-18 21:18:08 -0700

    adding sys path

--- src/helpers/xgb_predict.py
+++ src/helpers/xgb_predict.py
@@ -3,34 +3,24 @@
 #-or from a pandas matrix (probably numpy also):
 #should this be a daemon? Probably better than reloading everytime. 
 
+
+#TODO: change this to one or two methods, pass params as a dict
+
 import numpy as np
 import xgboost as xgb
 import pickle
 #the feature modules we'll need to build the DMatrix
 import sys
-sys.path.insert(0, "../ngrams_wordnet")
-import feature_length as fl
-import feature_synset as fs
-import feature_ngrams as fn
 
+def predict(x_test):
+	'''
+	expects an (x, 3) np matrix
+	returns a float prediction
+	'''
+	
+	loaded_model = pickle.load(open("../ngrams_wordnet/ngrams.pickle.dat", "rb"))
 
-questions = np.array(sys.argv[1:3])
-print(questions)
-
-
-length = fl.length_feature(questions[0], questions[1])
-ngrams = fn.ngram(questions[0], questions[1], 1)
-synset = fs.syns_feature(questions[0], questions[1])
-
-data = np.array([length, ngrams, synset])
-
-loaded_model = pickle.load(open("../ngrams_wordnet/ngrams.pickle.dat", "rb"))
-
-#is this correct?
-data = data.reshape(1,-1)
-#loading q1 and q2 into numpy array
-print(data.shape)
-
-data = xgb.DMatrix(data)
-prediction = loaded_model.predict(data)
-print("prediction is: ", prediction)
+	data = xgb.DMatrix(x_test)
+	prediction = loaded_model.predict(data)
+	print("prediction is: ", prediction)
+	return prediction
